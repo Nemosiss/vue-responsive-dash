86729b76467b5f11a03d1b2135f94902594d7cb9
Владимир Борисов <borisovv@live.ru>
2022-07-21 19:33:01 +0300

    Types added

---
+++ types/index.d.ts
@@ -0,0 +1,9 @@
+declare module 'responsive-dash-layout' {
+  import { Component } from 'vue';
+
+  const Dashboard: Component;
+  const DashItem: Component;
+  const DashLayout: Component;
+
+  export { Dashboard, DashItem, DashLayout };
+}
