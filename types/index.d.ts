60aa0f6c4ccb6d74299dcc734351c7ac383f836f
Владимир Борисов <borisovv@live.ru>
2022-07-21 19:30:20 +0300

    All bugs fixed

--- types/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'vue-responsive-dash' {
-  import { Component } from 'vue';
-
-  const Dashboard: Component;
-  const DashItem: Component;
-  const DashLayout: Component;
-
-  export { Dashboard, DashItem, DashLayout };
-}
